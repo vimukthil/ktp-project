522362df1412bd3c63a3c7acd46b2696a166d4ae
vimukthil <vimukthiliyanage@gmail.com>
2022-05-10 00:35:37 +0800

    feat: read and load from the JSON gist raw.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_restx import Api, Resource, fields
 from werkzeug.middleware.proxy_fix import ProxyFix
+import json
+from urllib.request import urlopen
+import certifi
+
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -29,29 +33,47 @@ item = api.model(
 class Item(object):
     def __init__(self):
         self.counter = 0
-        self.items = [
-            {"id": 1, "name": "W Series Plunger Pump", "price": 3290, "quantity": 100},
-            {"id": 2, "name": "Quickflange", "price": 12500, "quantity": 3},
-            {
-                "id": 3,
-                "name": "Solar Chemical Injection Pump",
-                "price": 5000,
-                "quantity": 5,
-            },
-        ]
+        self.items = []
+
+    def create(self, data):
+        item = data
+        item["id"] = self.counter = self.counter + 1
+        self.items.append(item)
+        return item
+
+    def preload(self):
+        """Load object from file."""
+        gist_raw_url = "https://gist.githubusercontent.com/chamathpali/7cccd0ff8a0338645559e5ed468231fa/raw/3a467ff8807a090cbdbe5e4583b8d07b925a7979/items.json"
+        response = urlopen(gist_raw_url, cafile=certifi.where())
+        json_response = json.load(response)
+
+        for res in json_response:
+            self.create(res)
+
+        return {"status": True, "message": "successful."}
+
+
+item_obj = Item()
 
 
 @ns.route("/")
 class ItemList(Resource):
-    """Shows a list of all items, and lets you POST to add new tasks"""
-
-    item_obj = Item()
+    """Shows a list of all items, and lets you POST to add new tasks."""
 
     @ns.doc("list_items")
     @ns.marshal_list_with(item)
     def get(self):
         """List all tasks"""
-        return self.item_obj.items
+        return item_obj.items
+
+
+@ns.route("/preload")
+class ItemAction(Resource):
+    """Load list of items."""
+
+    @ns.doc("load_items")
+    def get(self):
+        item_obj.preload()
 
 
 if __name__ == "__main__":
